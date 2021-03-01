17c1480bdcc3f32d512814dc632aded515d66226
Lily Vandenbore <lily.vandenbore@student.vives.be>
2021-03-01 20:34:14 +0100

    Adding CLI for program to project to use for testing

---
+++ PlantenApplicatie.CLI/Program.cs
@@ -0,0 +1,28 @@
+﻿using PlantenApplicatie.Domain;
+using System;
+using System.Linq;
+
+namespace PlantenApplicatie.CLI
+{
+    class Program
+    {
+        private static PlantenContext _context;
+
+        static void Main(string[] args)
+        {
+            _context = new PlantenContext();
+
+            PrintPlanten();
+        }
+
+        static void PrintPlanten()
+        {
+            var planten = _context.Planten2021.ToList();
+            
+            foreach (var plant in planten)
+            {
+                Console.WriteLine($"The plant name is {plant.Plantnaam}");
+            }
+        }
+    }
+}
