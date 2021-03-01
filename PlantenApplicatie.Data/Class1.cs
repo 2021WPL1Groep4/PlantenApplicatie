f80f21a6730a89f33d160acc9da93813acd5f00c
Lily Vandenbore <r0844224@student.vives.be>
2021-03-01 17:16:37 +0100

    Adding entity framework bindings

---
+++ PlantenApplicatie.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PlantenApplicatie.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ PlantenApplicatie.Domain/AbioBezonning.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlantenApplicatie.Domain
+{
+    public partial class AbioBezonning
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+    }
+}
