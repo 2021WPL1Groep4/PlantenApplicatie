c42e8a7137904d4e769788660d66c93dbc9c4826
LilyVandenbore <79832982+LilyVandenbore@users.noreply.github.com>
2021-03-08 14:14:06 +0100

    Update ConnectionConfig.cs

--- PlantenApplicatie.Data/ConnectionConfig.cs
+++ PlantenApplicatie.Data/ConnectionConfig.cs
@@ -1,11 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace PlantenApplicatie.Data
-{
-    public static class ConnectionConfig
-    {
-        public static string ConnectionString => "Server=LAPTOP-DAVYGEVA\\VIVES;Database=Planten2021;Trusted_Connection=True;";
-    }
-}
