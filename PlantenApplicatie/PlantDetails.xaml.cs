26bb1f21abcf1117c74d39d9b5c45b69c716c99b
jimchou <r0842584@student.vives.be>
2021-03-11 10:54:14 +0100

    backend

--- PlantenApplicatie/PlantDetails.xaml.cs
+++ PlantenApplicatie/PlantDetails.xaml.cs
@@ -15,16 +15,69 @@ namespace PlantenApplicatie
     /// <summary>
     /// Interaction logic for PlantDetails.xaml
     /// </summary>
+
     public partial class PlantDetails : Window
     {
         public PlantDetails()
         {
             InitializeComponent();
+            Start();
+        }
+
+        private void Start()
+        {
+            cmbEigenschappen.Items.Add("FenoType");
+            cmbEigenschappen.Items.Add("Abiotiek");
+            cmbEigenschappen.Items.Add("Beheer");
+            cmbEigenschappen.Items.Add("Extra's");
+            cmbEigenschappen.Items.Add("Commensalisme");
+
+            cmbEigenschappen.SelectedIndex = 0;
+
+
         }
 
         private void btnSluiten_Click(object sender, RoutedEventArgs e)
         {
             Close();
         }
+
+        private void cmbEigenschappen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectie();
+        }
+
+        private void selectie()
+        {
+            string comboboxText = cmbEigenschappen.SelectedValue.ToString();
+
+            switch (comboboxText)
+            {
+                case "FenoType":
+                    lstLijst.Visibility = Visibility.Visible;
+
+                    break;
+                case "Abiotiek":
+                    lstLijst.Visibility = Visibility.Collapsed;
+
+                    break;
+                case "Beheer":
+                    lstLijst.Visibility = Visibility.Visible;
+
+                    break;
+                case "Extra's":
+                    lstLijst.Visibility = Visibility.Collapsed;
+
+                    break;
+                case "Commensalisme":
+                    lstLijst.Visibility = Visibility.Collapsed;
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
 }
+
