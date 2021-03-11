c6f73a7cee215a6baeec2cf7eb8ae75d858c4ba9
jimchou <r0842584@student.vives.be>
2021-03-11 12:09:19 +0100

    Aanpassen aan nieuwe database

--- PlantenApplicatie.Domain/TfgsvSoort.cs
+++ PlantenApplicatie.Domain/TfgsvSoort.cs
@@ -10,10 +10,6 @@ namespace PlantenApplicatie.Domain
         public string Soortnaam { get; set; }
         public string NlNaam { get; set; }
 
-        // override ToString, toon de property waarde in de combobox
-        public override string ToString()
-        {
-            return Soortnaam;
-        }
+        public virtual TfgsvGeslacht GeslachtGeslacht { get; set; }
     }
 }
